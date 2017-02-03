d1bc079fe7eb7c455bce5ef61b406d2960c6b6bc
Squidly271 <azawadzki@arrowgames.com>
2017-02-03 13:47:46 -0500

    change syslog filename

--- source/fix.common.problems/usr/local/emhttp/plugins/fix.common.problems/scripts/starttail.sh
+++ source/fix.common.problems/usr/local/emhttp/plugins/fix.common.problems/scripts/starttail.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /boot/logs
 
-echo "tail -f /var/log/syslog > /boot/logs/syslog.txt & " | at NOW -M > /dev/null 2>&1
+echo "tail -f /var/log/syslog > /boot/logs/FCPsyslog_tail.txt & " | at NOW -M > /dev/null 2>&1
 echo "/usr/local/emhttp/plugins/fix.common.problems/scripts/startDiagnostics.php & " | at NOW -M > /dev/null 2>&1
 logger "Fix Common Problems: Troubleshooting mode activated"
 
