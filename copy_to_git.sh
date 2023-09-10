16eeb319b86536df0f26a537c7d82ddad5c12995
Squidly271 <unraidsquid@gmail.com>
2023-09-09 21:41:49 -0400

    don't check for macvlan on 612+

--- copy_to_git.sh
+++ copy_to_git.sh
@@ -3,5 +3,5 @@
 mkdir -p "/tmp/GitHub/fix.common.problems/source/fix.common.problems/usr/local/emhttp/plugins/fix.common.problems/"
 
 cp /usr/local/emhttp/plugins/fix.common.problems/* /tmp/GitHub/fix.common.problems/source/fix.common.problems/usr/local/emhttp/plugins/fix.common.problems -R -v -p
-
+find . -maxdepth 9999 -noleaf -type f -name "._*" -exec rm -v "{}" \;
 
--- source/fix.common.problems/usr/local/emhttp/plugins/fix.common.problems/include/tests.php
+++ source/fix.common.problems/usr/local/emhttp/plugins/fix.common.problems/include/tests.php
@@ -1376,6 +1376,8 @@ function inotifyExhausted() {
 function macvlan() {
 	global $fixPaths, $fixSettings, $autoUpdateOverride, $developerMode, $communityApplicationsInstalled, $dockerRunning, $ignoreList, $shareList, $unRaidVersion;
 
+	if (version_compare($unRaidVersion,"6.12.0",">"))
+		return;
 	exec("cat /var/log/syslog | grep -i 'Modules linked in:'",$out);
 	if ($out) {
 		foreach ($out as $output) {
